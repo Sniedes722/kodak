cfcf9808fb08a67b24b5359b0da00cbe777b9e6c
Shawn Niederriter <shawn.niederriter@temple.edu>
2017-02-25 23:05:16 +0000

    Fixed rest-client URL

--- clients/rest-client.py
+++ clients/rest-client.py
@@ -6,7 +6,7 @@ a = 6
 b = 7
 while x <= y:
     payload = {'n1': a,'n2': b}
-    r = requests.post("https://cpp-sandbox-sneed722.c9users.io:8081/api/v1/sum",data=payload)
+    r = requests.post("http://localhost/api/v1/sum",data=payload)
     print(r.json())
     a = a+b
     b = b+a
