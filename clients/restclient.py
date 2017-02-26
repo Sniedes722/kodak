909d7e9df4c8da8f868d6b16cb3d7c640645d08a
Shawn Niederriter <shawn.niederriter@temple.edu>
2017-02-26 01:40:26 +0000

    Added front end

--- clients/restclient.py
+++ clients/restclient.py
@@ -1,13 +1,11 @@
 import requests
 
 x = 0
-y = 100
+y = 10
 a = 6
 b = 7
 while x <= y:
     payload = {'n1': a,'n2': b}
-    r = requests.post("http://localhost/api/v1/sum",data=payload)
+    r = requests.post("https://kodak-sneed722.c9users.io:8081/api/v1/sum",data=payload)
     print(r.json())
-    a = a+b
-    b = b+a
     x = x + 1
